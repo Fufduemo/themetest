7e481035298ce3b5f0744ef8a8caf7f5166c7859
Fufduemo <femog99@gmail.com>
2023-05-03 16:19:27 -0400

    Update appNav.js

--- themere/navigation/appNav.js
+++ themere/navigation/appNav.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../Screens/HomeScreen";
 import ProfileScreen from "../Screens/ProfileScreen";
+import themeContext from "../themes/themeContext";
 
 export default function AppNav() {
   const Stack = createStackNavigator();
+  const theme = useContext(themeContext);
 
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: "Home",
+          headerStyle: {
+            backgroundColor: theme.backgroundColor,
+          },
+          headerTintColor: theme.color,
+          tintColor: theme.color,
+        }}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          title: "Profile",
+          headerStyle: {
+            backgroundColor: theme.backgroundColor,
+          },
+          headerTintColor: theme.color,
+          tintColor: theme.color,
+        }}
+      />
     </Stack.Navigator>
   );
 }
